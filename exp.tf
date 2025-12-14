d1eeddcda50734da60c225d1086079cab341dbc3
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2025-12-14 19:41:26 +0100

    Add Google Cloud storage bucket and compute instance

---
+++ exp.tf
@@ -0,0 +1,28 @@
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "r3ap3rpygcpbucket"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+
+  name         = "terraform-instance-2"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+}
